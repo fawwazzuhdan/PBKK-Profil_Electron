ebf3416a12eeaf641b23a6e32cd5d51cbe90ac58
fawwazzuhdan <fawwazzuhdan@gmail.com>
2020-02-13 02:35:13 +0700

    update color

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const BrowserWindow = electron.BrowserWindow;
 
 var mainWindow;
 app.on('ready',function(){
-	mainWindow = new BrowserWindow({width: 1024, height: 768,backgroundColor: '#2e2c29'});
+	mainWindow = new BrowserWindow({width: 1024, height: 768,backgroundColor: '#ffffff'});
 	mainWindow.loadURL(url.format({
 		pathname: path.join(__dirname, 'index.html'),
 		protocol: 'file:',
